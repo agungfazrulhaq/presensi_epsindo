15c55b0491bdd902519e85f41e90a289189f48af
agungfazrulhaq <agungfazrulhaq95@gmail.com>
2022-08-26 10:47:28 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+twelvemonth = ['January','February','March','April','May']
+
+@app.route('/')
+def index():
+    page_info = {'page':'index', 'months':twelvemonth}
+    return render_template('index.html', result = page_info)
+
+@app.route('/monthly')
+def monthly():
+    page_info = {'page':'monthly', 'month':'January', 'months':twelvemonth}
+    return render_template('monthly.html', result = page_info)
+
+@app.route('/importdata')
+def import_data():
+    page_info = {'page':'importdata', 'months':twelvemonth}
+    return render_template('importdata.html', result=page_info)
